4ff2bc30bf190b21e57c407b72be351b6f83a733
Leonard94 <vladisla.koleda@yandex.ru>
2021-11-18 16:23:44 +0300

    Сделал небольшой рефакторинг кода

--- js/script.js
+++ js/script.js
@@ -8,65 +8,7 @@ const passwordRepeat = document.getElementById('passwordRepeat')
 const checkbox = document.getElementById('checkbox')
 
 // Счётчик ошибок в полях
-let error = 0
-
-// Ошибочное заполнение формы
-const showError = (input, message) => {
-    const formField = input.parentElement
-    formField.classList.remove('success')
-    formField.classList.add('error')
-    const small = formField.querySelector('small')
-    small.innerHTML = message
-    small.style.visibility = 'visible'
-    return error++
-}
-
-// Успешное заполнение формы
-const showSuccess = (input) => {
-    const formField = input.parentElement
-    formField.classList.remove('error')
-    formField.classList.add('success')
-    formField.querySelector('small').style.visibility = 'hidden'
-}
-
-// Проверка введёной почты
-const checkEmail = (input) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(String(input.value.trim()).toLowerCase())) {
-        showSuccess(input)
-    } else {
-        showError(input, 'Введите корректный email')
-    }
-}
-
-// Проверка на пустоту
-const checkRequired = (inputs) => {
-    inputs.forEach((input) => {
-        if (input.value.trim() === '') showError(input, `${input.name} - обязательное поле`)
-        else showSuccess(input)
-    })
-}
-
-// Проверка минимальной длины поля
-const checkInputLenght = (inputs, min) => {
-    inputs.forEach((input) => {
-        if (input.value.length < min && input.value.length != 0) showError(input, `Поле «${input.name}» должно содержать не менее ${min} символов`)
-    })
-}
-
-// Сравнение паролей
-const checkPasswordMatch = (password, passwordRepeat) => {
-    if (password.value !== passwordRepeat.value) {
-        showError(password, 'Пароли не совпадают')
-        showError(passwordRepeat, 'Пароли не совпадают')
-    }
-}
-
-// Нажат ли checkbox
-const isChecked = (input) => {
-    if (input.checked === true) showSuccess(input)
-    else showError(input, 'Нужно согласиться на обработку персональных данных')
-}
+let errorCounter = 0
 
 // Observer submit
 form.addEventListener('submit', (e) => {
@@ -80,7 +22,7 @@ form.addEventListener('submit', (e) => {
     formValidate()
 
     // Если ошибок нет, отправляем форму
-    if (error === 0) {
+    if (errorCounter === 0) {
         alert('Ошибок нет, форму отправляем на сервер')
         // const formData = new FormData(form);
     }
@@ -93,8 +35,8 @@ const formValidate = () => {
     // Проверяем все инпуты на пустоту
     checkRequired([login, phone, password, passwordRepeat])
 
-    // Проверяем все инпуты на количество символов
-    checkInputLenght([login, phone, password, passwordRepeat], 5)
+    // Проверяем все инпуты на минимальное количество символов
+    checkMinCharacters([login, phone, password, passwordRepeat], 5)
 
     // Сравнение паролей
     checkPasswordMatch(password, passwordRepeat)
@@ -105,6 +47,57 @@ const formValidate = () => {
     // Проверка нажатого checkbox
     isChecked(checkbox)
 
+}
+
+// Проверка на пустоту
+const checkRequired = (inputs) => {
+    inputs.forEach((input) => {
+        if (input.value.trim() === '') showError(input, `${input.name} - обязательное поле`)
+        else showSuccess(input)
+    })
+}
+
+// Проверка на минимальное количество символов в поле
+const checkMinCharacters = (inputs, min) => {
+    inputs.forEach((input) => {
+        if (input.value.length < min && input.value.length != 0) showError(input, `Поле «${input.name}» должно содержать не менее ${min} символов`)
+    })
+}
+
+// Сравнение паролей
+const checkPasswordMatch = (password, passwordRepeat) => {
+    if (password.value !== passwordRepeat.value) showError(passwordRepeat, 'Пароли не совпадают')
+}
+
+// Валидация email
+const checkEmail = (input) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(input.value.trim()).toLowerCase())) showError(input, 'Введите корректную почту')
+}
+
+// Нажат ли checkbox
+const isChecked = (input) => {
+    if (input.checked !== true) showError(input, 'Нужно согласиться на обработку персональных данных')
+    else showSuccess(input)
+}
+
+// Ошибочное заполнение формы
+const showError = (input, message) => {
+    const parent = input.parentElement
+    const small = parent.querySelector('small')
+    parent.classList.remove('success')
+    parent.classList.add('error')
+    small.innerHTML = message
+    small.style.visibility = 'visible'
+    errorCounter++
+}
+
+// Успешное заполнение формы
+const showSuccess = (input) => {
+    const parent = input.parentElement
+    parent.querySelector('small').style.visibility = 'hidden'
+    parent.classList.remove('error')
+    parent.classList.add('success')
 }
 
 // Обработчик поля телефон. Запрет на ввод букв
